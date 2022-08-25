988c9105fbdbd7836dc51302b3576da201b55052
EC2 Default User <ec2-user@ip-172-31-75-174.ec2.internal>
2022-08-25 13:45:03 +0000

    update project

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,10 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+docker build --tag=p4 .
 
 # Step 2: 
-# List docker images
+docker image ls
 
-# Step 3: 
-# Run flask app
+# Step 3:
+docker run -p 8000:8080 p4
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=fnim987/p4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag 0e78b9a98dfb $dockerpath:v1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1.0.0
